a89c8681d256ea71fcb3c2eaaae0e28919e92a78
Tsabitah59 <tsabitahsafa06@gmail.com>
2024-05-06 08:58:13 +0700

    JUHAAAAA

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ crsf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Zest Mart Admin</title>
 
